3cd2106f3f9cbbb4cbfa54d72a29619165a686ca
Lengyel Dániel <lengyel.daniel.21i@egresy.info>
2025-03-25 10:36:48 +0100

    Add project files.

---
+++ WPF_gyakorlas/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_gyakorlas
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<string> names = new List<string>();
+        List<int> ages = new List<int>();
+        ServerController sc = new ServerController();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            UpdateAges();
+            UpdateNames();
+        }
+
+        private async void Add_Click(object sender, RoutedEventArgs e)
+        {
+            await sc.CreatePerson(CreateNameTB.Text, Convert.ToInt32(CreateAgeTB.Text));
+            UpdateAges();
+            UpdateNames();
+        }
+
+
+        private async void UpdateNames()
+        {
+            names.Clear();
+            NameSp.Children.Clear();
+            names = await sc.getAllNames();
+
+            if (names.Count > 0)
+            {
+                for (int i = 0; i < names.Count; i++)
+                {
+                    NameSp.Children.Add(new TextBlock() { Text = names[i], FontSize = 20, Foreground = Brushes.White, HorizontalAlignment = HorizontalAlignment.Center });
+                }
+            }
+
+        }
+
+
+        private async void UpdateAges()
+        {
+            ages.Clear();
+            AgeSp.Children.Clear();
+            ages = await sc.getAllages();
+
+            if (ages.Count > 0)
+            {
+                for (int i = 0; i < ages.Count; i++)
+                {
+                    AgeSp.Children.Add(new TextBlock() { Text = ages[i].ToString(), FontSize = 20, Foreground = Brushes.White, HorizontalAlignment = HorizontalAlignment.Center });
+                }
+            }
+
+        }
+
+        private async void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            await sc.DeletePersonAsync(CreateNameTB.Text);
+            UpdateAges();
+            UpdateNames();
+        }
+    }
+}
---
+++ WPF_gyakorlas/ServerController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Newtonsoft.Json;
+
+namespace WPF_gyakorlas
+{
+    internal class ServerController
+    {
+
+        HttpClient client = new HttpClient();
+
+        public ServerController()
+        {
+
+        }
+
+
+        public async Task<List<string>> getAllNames()
+        {
+            List<string> list = new List<string>();
+            string nameURL = "http://localhost:3000/allName";
+
+            try
+            {
+                    HttpResponseMessage response = await client.GetAsync(nameURL);
+                    response.EnsureSuccessStatusCode();
+
+                    string result = await response.Content.ReadAsStringAsync(); 
+                    list = JsonConvert.DeserializeObject<List<JsonData>>(result).Select(item => item.name).ToList();
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hiba történt: " + ex.Message); 
+            }
+
+            return list;
+        }
+
+
+
+        public async Task DeletePersonAsync(string name)
+        {
+            string url = "http://localhost:3000/deletePerson";
+
+            var data = new { name = name };
+
+            string jsonContent = JsonConvert.SerializeObject(data);
+
+            var content = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+
+            try
+            {
+                var response = await client.PostAsync(url, content);
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    MessageBox.Show(responseBody);
+                }
+                else
+                {
+                    MessageBox.Show("Hiba történt a törlés során. HTTP státuszkód: " + response.StatusCode);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hiba: " + ex.Message);
+            }
+        }
+
+        public async Task CreatePerson(string name, int age)
+        {
+            string registerURL = "http://localhost:3000/createPerson";
+
+            try
+            {
+                    var personData = new
+                    {
+                        name = name,
+                        age = age
+                    };
+                    string jsonData = JsonConvert.SerializeObject(personData);
+                    StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+                    HttpResponseMessage response = await client.PostAsync(registerURL, content);
+
+                    string responseBody = await response.Content.ReadAsStringAsync();
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        MessageBox.Show(responseBody);
+                    }
+                    else
+                    {
+                        string errorMessage = await response.Content.ReadAsStringAsync();
+                        MessageBox.Show("Hiba történt az emberkísérletek során:" + errorMessage);
+                    }
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hiba történt: " + ex.Message);
+            }
+        }
+
+
+        public async Task<List<int>> getAllages()
+        {
+            List<int> list = new List<int>();
+            string nameURL = "http://localhost:3000/allAge";
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage response = await client.GetAsync(nameURL);
+                    response.EnsureSuccessStatusCode();
+
+                    string result = await response.Content.ReadAsStringAsync();
+                    list = JsonConvert.DeserializeObject<List<JsonData>>(result).Select(item => item.age).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hiba történt: " + ex.Message);
+            }
+
+            return list;
+        }
+    }
+    class JsonData
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public string message { get; set; }
+    }
+}
